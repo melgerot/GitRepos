8b6d0168b99558e4cdbe068c3233c47badb61ed3
Martin Elgerot <melgerot@hotmail.com>
2013-05-07 20:32:02 +0200

    CustomCell Added

---
+++ Mobile.SAP.CRM/MT.SAP.CRM/obj/Xcode/8/CustomerActivityCell.m
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "CustomerActivityCell.h"
+
+@implementation CustomerActivityCell
+
+- (void)dealloc {
+    [_TitleLabel release];
+    [_SubTitle1Label release];
+    [_SubTitle2Label release];
+    [super dealloc];
+}
+@end
